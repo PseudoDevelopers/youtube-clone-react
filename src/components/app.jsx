cab2187d4e23fc4e2271fed3dc2d38fd25db38d9
PSEUDO <Pseudo.Developers@gmail.ccom>
2020-10-16 17:28:51 +0500

    Ready to start creating App

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class App extends React.Component {
+    state = {  }
+    render() {
+        return (
+            <p>App</p>
+        )
+    }
+}
+
+export default App
